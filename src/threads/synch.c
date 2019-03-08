893059fada87b28aa0739f8e9f1b2d511ad322aa
RHowe22 <robjhowe22@gmail.com>
2019-03-08 16:55:38 -0500

    set up condition comparator

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -302,7 +302,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  list_insert_ordered(&cond->waiters, &waiter.elem, condComp, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
@@ -342,4 +342,10 @@ cond_broadcast (struct condition *cond, struct lock *lock)
 
   while (!list_empty (&cond->waiters))
     cond_signal (cond, lock);
+}
+
+bool condComp( struct list_elem * a, struct list_elem * b, void * useless UNUSED){
+  struct semaphore_elem * aSem = list_entry(a, struct semaphore_elem, elem );
+  struct semaphore_elem * bSem = list_entry(b, struct semaphore_elem, elem );
+  return (aSem->semaphore.firstPriority) < (bSem->semaphore.firstPriority);
 }
