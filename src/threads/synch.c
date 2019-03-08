2ed48a8d2b70f550d8920e70b42a7bd84f3dace0
RHowe22 <robjhowe22@gmail.com>
2019-03-08 16:30:23 -0500

    priority sema now works

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -109,15 +109,22 @@ void
 sema_up (struct semaphore *sema) 
 {
   enum intr_level old_level;
-
+  struct thread * front = NULL;
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
-  sema->value++;
+    thread_unblock (front=( list_entry (list_pop_front (&sema->waiters),
+                                struct thread, elem)));
+
+
+  sema->value++;                              
   intr_set_level (old_level);
+  if(front !=NULL){
+    if(!intr_context()){thread_yield();}
+    else { intr_yield_on_return();}
+  }
+  
 }
 
 static void sema_test_helper (void *sema_);
